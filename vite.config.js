56113c514be336a7725d3257df188b115b606677
Sauvik Chatterjee <bengalcodingacademy@gmail.com>
2025-09-14 12:22:02 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // ensures Netlify serves correctly from root
+  build: {
+    outDir: 'dist', // default, but keep it explicit
+  },
 })
